bcbb199dfb59965ce5555a39dc6c88b68ec1d14d
thesecretnix <59189977+thesecretnix@users.noreply.github.com>
2020-10-11 05:00:16 +0530

    Sidebar component set up done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import About from './components/About';
+import Contact from './components/Contact';
+import Introduction from './components/Introduction';
+import Projects from './components/Projects';
+import Sidebar from './components/Sidebar';
 
 export default class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div id="colorlib-page">
+        <div id="container-wrap">
+         	
+           <Sidebar></Sidebar>
+
+          {/* <div id="colorlib-main">
+            <Introduction></Introduction>
+            <About></About>
+            <Projects></Projects>
+            <Contact></Contact>
+          </div> */}
+
+      	</div>
       </div>
     )
   }
