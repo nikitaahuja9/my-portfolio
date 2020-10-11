fb0fbda06ca5e7edfccfe19cce7278c92fbf3962
thesecretnix <59189977+thesecretnix@users.noreply.github.com>
2020-10-11 23:43:27 +0530

    Basic template ready!

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -1,11 +1,6 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 export default class Introduction extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {about:false};
-  }
 
   render() {
     return (
@@ -13,41 +8,40 @@ export default class Introduction extends Component {
         <section id="colorlib-hero" className="js-fullheight" data-section="home">
           <div className="flexslider js-fullheight">
             <ul className="slides">
-              {this.state.about == false ? 
-              <li style={{backgroundImage: 'url(images/background.jpg)'}}>
-                <div className="overlay" />
-                <div className="container-fluid">
-                  <div className="row">
-                    <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
-                      <div className="slider-text-inner js-fullheight">
-                        <div className="desc">
-                          <h1>Hi! I'm Nikita</h1>
-                        </div>  
+                <li style={{ backgroundImage: 'url(images/background.jpg)' }}>
+                  <div className="overlay" />
+                  <div className="container-fluid">
+                    <div className="row">
+                      <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
+                        <div className="slider-text-inner js-fullheight">
+                          <div className="desc">
+                            <h1>Hi! I'm Nikita</h1>
+                          </div>
+                        </div>
                       </div>
                     </div>
                   </div>
-                </div>
-              </li> : null }
-              
-              <li style={{backgroundImage: 'url(images/background.jpg)'}}>
+                </li> 
+
+              <li style={{ backgroundImage: 'url(images/background.jpg)' }}>
                 <div className="overlay" />
                 <div className="container-fluid">
                   <div className="row">
                     <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
                       <div className="slider-text-inner">
                         <div className="desc">
-                          <h1>Who am I?</h1>   
-                          
-                          <p><a className="btn btn-primary btn-learn" 
-                          data-nav-section="about" href="about"
-                          >About Me <i className="icon-user"/></a></p>
+                          <h1>A little about me..</h1>
+
+                          <p><a className="btn btn-primary btn-learn"
+                            data-nav-section="about"
+                          >View Resume <i className="icon-download4" /></a></p>
                         </div>
                       </div>
                     </div>
                   </div>
                 </div>
               </li>
-              <li style={{backgroundImage: 'url(images/background.jpg)'}}>
+              <li style={{ backgroundImage: 'url(images/background.jpg)' }}>
                 <div className="overlay" />
                 <div className="container-fluid">
                   <div className="row">
@@ -55,22 +49,22 @@ export default class Introduction extends Component {
                       <div className="slider-text-inner">
                         <div className="desc">
                           <h1>Check out my work</h1>
-                          <p><a className="btn btn-primary btn-learn" href="/projects" >View Projects <i className="icon-briefcase3" /></a></p>
+                          <p><a className="btn btn-primary btn-learn" href="https://github.com/nikitaahuja9" target="_blank" rel="noopener noreferrer">View Github <i className="icon-briefcase3" /></a></p>
                         </div>
                       </div>
                     </div>
                   </div>
                 </div>
               </li>
-              <li style={{backgroundImage: 'url(images/background.jpg)'}}>
+              <li style={{ backgroundImage: 'url(images/background.jpg)' }}>
                 <div className="overlay" />
                 <div className="container-fluid">
                   <div className="row">
                     <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
                       <div className="slider-text-inner">
                         <div className="desc">
-                          <h1>Hit me up</h1>
-                          <p><a className="btn btn-primary btn-learn" href="/contact">Contact Me <i className="icon-phone" /></a></p>
+                          <h1>Connect with me</h1>
+                          <p><a className="btn btn-primary btn-learn" href="https://linkedin.com/in/nikitaahuja9" target="_blank" rel="noopener noreferrer">View LinkedIn <i className="icon-linkedin2" /></a></p>
                         </div>
                       </div>
                     </div>
