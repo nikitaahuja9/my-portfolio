c08085aeef2d3d260902e731bfe8fe4a79b7227e
thesecretnix <59189977+thesecretnix@users.noreply.github.com>
2020-12-14 17:22:04 +0530

    Background changed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 export default class About extends Component {
 
@@ -6,27 +6,39 @@ export default class About extends Component {
     return (
       <div>
         <section className="colorlib-about" data-section="about">
-        <div className="colorlib-narrow-content">
+          <div className="colorlib-narrow-content">
             <div className="row">
-            <div className="col-md-12">
+              <div className="col-md-12">
                 <div className="row row-bottom-padded-sm animate-box" data-animate-effect="fadeInLeft">
-                <div className="col-md-12">
+                  <div className="col-md-12">
                     <div className="about-desc">
-                    <h2 className="colorlib-heading">About Me</h2>
-                    <p><i><q>Don't work for recognition but do work worthy of recognition.</q> -Henry Jackson Brown, Jr</i></p>
-                    <p>Hi there! I'm Nikita.<br/>
-                      I'm a Computer Science Engineer and a technology driven enthusiast, whose always seeking new opportunities to devise innovative solutions that positively impact people's lives.</p>
-                    <p>Being a recent graduate from BITS Pilani Dubai, I have worked on a handful of academic and personal projects, that showcase my interest in the field of development. I have 8 months of professional work experience and profound knowledge in all phases of the software development lifecycle - from business requirement analysis to design, implementation and testing.</p>
-                    <p>In addition, I have strong communication, collaboration and team building skills, with proficiency in grasping new technical concepts and utilizing the same in a productive manner.</p>
-                    <p>In my free time, I enjoy exercising, reading novels and watching documentaries. Reach out to me for networking and discussing new opportunities!</p>
+                      <h2 className="colorlib-heading">About Me</h2>
+                      <p><i><q>Don't work for recognition but do work worthy of recognition.</q> -Henry Jackson Brown, Jr</i></p>
+                      <p>Hi there! I'm Nikita.<br />
+
+                      <br/>
+                     I'm a CS Undergraduate from BITS Pilani Dubai with almost a year of work experience, always seeking new opportunities to devise innovative software solutions that positively impact people’s lives. 
+                     A motivated and passionate individual with the necessary drive to make a mark in the big picture!</p>
+
+
+                      <p>Languages I use: </p>
+
+                      <p>
+                      <a href="https://www.java.com" target="_blank"> <img src="https://devicon.dev/devicon.git/icons/java/java-original-wordmark.svg" alt="java" width="40" height="40" /> </a>   <a href="https://www.w3schools.com/js/default.asp" target="_blank"> <img src="https://devicon.dev/devicon.git/icons/javascript/javascript-original.svg" alt="javascript" width="40" height="40" /> </a>   <a href="https://www.w3schools.com/html/" target="_blank"> <img src="https://devicon.dev/devicon.git/icons/html5/html5-original.svg" alt="html" width="40" height="40" /></a>   <a href="https://www.w3schools.com/css/default.asp" target="_blank"> <img src="https://devicon.dev/devicon.git/icons/css3/css3-original.svg" alt="css" width="40" height="40" /></a> <a href="https://sass-lang.com/" target="_blank"> <img src="https://devicon.dev/devicon.git/icons/sass/sass-original.svg" alt="sass" width="40" height="40" /></a>   <a href="https://jquery.com/" target="_blank"> <img src="https://devicon.dev/devicon.git/icons/jquery/jquery-original-wordmark.svg" alt="sass" width="40" height="40" /></a>   <a href="https://docs.microsoft.com/en-us/dotnet/csharp/" target="_blank"> <img src="https://devicon.dev/devicon.git/icons/csharp/csharp-original.svg" alt="csharp" width="40" height="40" /></a>   <a href="https://www.typescriptlang.org/" target="_blank"> <img src="https://devicon.dev/devicon.git/icons/typescript/typescript-original.svg" alt="typescript" width="40" height="40" /></a>   <a href="https://www.python.org" target="_blank"> <img src="https://devicons.github.io/devicon/devicon.git/icons/python/python-original.svg" alt="python" width="40" height="40" /> </a>   <a href="https://www.php.net" target="_blank"> <img src="https://devicons.github.io/devicon/devicon.git/icons/php/php-original.svg" alt="php" width="40" height="40" /> </a>   <a href="https://developer.apple.com/swift/" target="_blank"> <img src="https://devicon.dev/devicon.git/icons/swift/swift-original.svg" alt="swift" width="40" height="40" /> </a>
+                      </p>
+
+                      <br/>
+
+                      <p>In my free time, I enjoy exercising, reading novels and watching documentaries. Reach out to me for collaborating on cool projects and discussing new opportunities!</p>
+                      <p>📫 nikita.ahuja40@gmail.com</p>
                     </div>
+                  </div>
                 </div>
-                </div>
+              </div>
             </div>
-            </div>
-        </div>
+          </div>
         </section>
-      </div> 
+      </div>
     )
   }
 }
--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -8,14 +8,14 @@ export default class Introduction extends Component {
         <section id="colorlib-hero" className="js-fullheight" data-section="home">
           <div className="flexslider js-fullheight">
             <ul className="slides">
-                <li style={{ backgroundImage: 'url(images/background.jpg)' }}>
+                <li style={{ backgroundImage: 'url(images/nikita-ahuja.gif)' }}>
                   <div className="overlay" />
                   <div className="container-fluid">
                     <div className="row">
                       <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
                         <div className="slider-text-inner js-fullheight">
                           <div className="desc">
-                            <h1>Hi! I'm Nikita</h1>
+                            <h1>Hi there! I'm Nikita</h1>
                           </div>
                         </div>
                       </div>
@@ -23,7 +23,7 @@ export default class Introduction extends Component {
                   </div>
                 </li> 
 
-              <li style={{ backgroundImage: 'url(images/background.jpg)' }}>
+              {/* <li style={{ backgroundImage: 'url(images/background.jpg)' }}>
                 <div className="overlay" />
                 <div className="container-fluid">
                   <div className="row">
@@ -40,8 +40,8 @@ export default class Introduction extends Component {
                     </div>
                   </div>
                 </div>
-              </li>
-              <li style={{ backgroundImage: 'url(images/background.jpg)' }}>
+              </li> */}
+              <li style={{ backgroundImage: 'url(images/nikita-ahuja.gif)' }}>
                 <div className="overlay" />
                 <div className="container-fluid">
                   <div className="row">
@@ -56,7 +56,7 @@ export default class Introduction extends Component {
                   </div>
                 </div>
               </li>
-              <li style={{ backgroundImage: 'url(images/background.jpg)' }}>
+              <li style={{ backgroundImage: 'url(images/nikita-ahuja.gif)' }}>
                 <div className="overlay" />
                 <div className="container-fluid">
                   <div className="row">
